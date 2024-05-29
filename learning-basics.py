9b74a440b1e7c7d9136a4d8719f5292a83f92d63
devangtomar <devangtomar123@gmail.com>
2024-05-29 23:52:02 +0530

    update learning-basics.py

--- learning-basics.py
+++ learning-basics.py
@@ -8,8 +8,10 @@ def add(x, y):
 def even(x):
     return x % 2 == 0
 
+
 def multiple(x):
     return x * 5
+
 
 fruits = [1, 2, 3, 4, 5]
 
@@ -23,3 +25,45 @@ print(fruits)
 print(reduce_add)
 print(list(filter(even, fruits)))
 print(list(map_multiply_by_5))
+
+# easy loop
+
+print([i for i in fruits])
+print([i for i in fruits if i % 2 == 0])
+
+# Decorator..
+
+
+def decorator(func):
+    def wrapper():
+        print("This is the start!")
+        func()
+        print("This is the end!")
+
+    return wrapper
+
+
+@decorator
+def print_something():
+    print("This is a function!")
+
+
+print_something()
+
+# Generator..
+
+
+# Define a generator function that yields the square of each number in the input list
+def square_generator(numbers):
+    for num in numbers:
+        yield num**2
+
+
+# Input list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Using the square_generator to generate squares of numbers
+squared_nums = square_generator(numbers)
+example = lambda x : x ** 2
+print([x for x in squared_nums])
+print(example(4))
