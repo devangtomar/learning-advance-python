649f85cb51f9738d15407aae94c7cfee435bdd61
devangtomar <devangtomar123@gmail.com>
2024-10-21 12:24:19 +0530

    Adding list, dictionary, sets and tuples!

---
+++ brocode/lists-sets-tuples.py
@@ -0,0 +1,24 @@
+a = []  # list
+b = {}  # dictionary or set
+c = ()  # tuple
+print(type(a))
+print(type(b))
+print(type(c))
+
+# List
+a = [1, 2, 3, 4, 5]
+print(a)
+print(a[0])
+print(a[1])
+
+# Dictionary
+b = {"name": "John", "age": 25}
+print(b)
+print(b["name"])
+print(b["age"])
+
+# Tuple
+c = (1, 2, 3, 4, 5)
+print(c)
+print(c[0])
+print(c[1])
