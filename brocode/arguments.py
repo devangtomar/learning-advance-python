de44d367cf9b8000f59c1d5841650cfc5c8c915a
devangtomar <devangtomar123@gmail.com>
2024-10-30 12:53:17 +0530

    Adding *args and **kwargs!

--- brocode/arguments.py
+++ brocode/arguments.py
@@ -21,9 +21,51 @@ def net_price(price, *, tax=0.07):
 
 print(net_price(100))  # 107.0
 
+
 def hello(greeting, name):
     print(f"{greeting}, {name}!")
+
 
 hello("Hello", "Alice")  # Hello, Alice!
 hello("Hello", name="Alice")  # Hello, Alice!
 hello(greeting="Hello", name="Alice")  # Hello, Alice!
+
+# arbitrary number of arguments
+
+
+def my_function(*args):
+    print(type(args))
+    print(args, sep="-")
+    for arg in args:
+        print(arg)
+
+
+my_function(1, 2, 3, 4, 5)
+
+# **kwargs
+
+
+def my_function(**kwargs):
+    print(type(kwargs))
+    print(kwargs, sep="-")
+    for key, value in kwargs.items():
+        print(key, value, sep="-")
+
+
+my_function(a=1, b=2, c=3)
+
+# combining number of arguments of positional, keyword, *args, and **kwargs
+
+
+def my_function(*args, **kwargs):
+    print(type(args))
+    print(type(kwargs))
+    print(args, sep="-")
+    print(kwargs, sep="-")
+    for arg in args:
+        print(arg)
+    for key, value in kwargs.items():
+        print(key, value, sep="-")
+
+
+my_function(1, 2, 3, 4, 5, a=1, b=2, c=3)
