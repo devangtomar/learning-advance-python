49333a48bee3ea6578d8880303cab12db02d58b0
devangtomar <devangtomar123@gmail.com>
2024-11-06 13:42:13 +0530

    Adding OOPS concept!

--- brocode/main.py
+++ brocode/main.py
@@ -1,5 +1,15 @@
+from car import Car
+
 def main():
     print("This is a main function!")
 
 if __name__ == "__main__":
     main()
+
+    car1 = Car("BMW", "2025", "Blue", True)
+    car2 = Car("Mustang", "2024", "Red", True)
+
+    print(car1)
+    print(car2)
+    print(car1.color)
+    print(car2.color)
