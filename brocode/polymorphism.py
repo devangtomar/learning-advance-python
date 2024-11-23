586d895ba94a2ec8d6cddbb1afb08a896131a1f2
devangtomar <devangtomar123@gmail.com>
2024-11-23 14:59:53 +0530

    Adding @property as well as decorators

---
+++ brocode/polymorphism.py
@@ -0,0 +1,10 @@
+# Polymorphism = Greek word that means to "have many forms or faces"
+# Poly = Many, Morphe = Form
+# TWO WAYS TO ACHIEVE POLYMORPHISM
+# 1. Inheritance = An object could be treated of the same type as a parent class
+# 2. "Duck typing" = Object must have necessary attributes/methods
+
+
+# "Duck typing" = Another way to achieve polymorphism besides Inheritance
+# Object must have the minimum necessary attributes/methods
+# "If it looks like a duck and quacks like a duck, it must be a duck."
