06ce450ea5012ae729bd771fbbcffa4cea93922c
devangtomar <devangtomar123@gmail.com>
2024-11-28 13:51:31 +0530

    Adding multithreading!

--- brocode/decorators.py
+++ brocode/decorators.py
@@ -28,6 +28,7 @@ class Rectangle:
         del self._width
         print("Width has been deleted!")
 
+
 rectangle = Rectangle(3, 4)
 rectangle.width = 0
 print(rectangle.width)
@@ -36,3 +37,24 @@ print(rectangle._width)
 print(rectangle._height)
 
 del rectangle.width
+
+# Decorator = A function that extends the behavior of another function
+# w/o modifying the base function
+# Pass the base function as an argument to the decorator
+# @add_sprinkles get_ice_cream("vanilla")
+
+
+def add_sprinkles(func):
+    def wrapper():
+        print("*You adding sprinkles!*")
+        func()
+
+    return wrapper
+
+
+@add_sprinkles
+def get_ice_cream():
+    print("Here is your ice cream")
+
+
+get_ice_cream()
